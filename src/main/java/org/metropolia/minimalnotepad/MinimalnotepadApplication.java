5581c51e661cfe46eb619e9cf98570405d8c3c41
Minimal-Alexi <alexpop@metropolia.fi>
2025-01-17 16:44:01 +0200

    Initial setup.

---
+++ src/main/java/org/metropolia/minimalnotepad/MinimalnotepadApplication.java
@@ -0,0 +1,13 @@
+package org.metropolia.minimalnotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalnotepadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalnotepadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/metropolia/minimalnotepad/MinimalnotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.metropolia.minimalnotepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalnotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
