f6d2d998fb1500bad4b565f19315e33cfd9203be
Minimal-Alexi <alexpop@metropolia.fi>
2025-01-31 14:32:26 +0200

    Creating requests.

---
+++ src/main/java/org/metropolia/minimalnotepad/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package org.metropolia.minimalnotepad.dto;
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String username;
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
