89d8e60f3e94cfd38cf6b75505f1502010f27a6b
Minimal-Alexi <alexpop@metropolia.fi>
2025-01-31 14:18:23 +0200

    Fixed username Added all JwtUtils.

--- src/main/java/org/metropolia/minimalnotepad/utils/JwtUtils.java
+++ src/main/java/org/metropolia/minimalnotepad/utils/JwtUtils.java
@@ -1,9 +1,12 @@
 package org.metropolia.minimalnotepad.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.metropolia.minimalnotepad.model.User;
 
 import java.util.Date;
+import java.util.function.Function;
 
 public class JwtUtils {
     // nrMilliseconds * nrSeconds * nrMinutes * nrHours * nrDays
@@ -15,8 +18,37 @@ public class JwtUtils {
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + timeValidity))
-                .signWith(SignatureAlgorithm.ES256,secretKey)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpirationDate(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpirationDate(token).before(new Date());
+    }
+
+    public boolean validateToken(String token, User user) {
+        return (user.getUsername().equals(extractUsername(token)) && !isTokenExpired(token));
     }
 
 }
