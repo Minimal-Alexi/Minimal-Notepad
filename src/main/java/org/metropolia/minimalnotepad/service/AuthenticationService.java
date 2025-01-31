4eabcd1e5d5674a5d24bad4894c54085371b6707
Minimal-Alexi <alexpop@metropolia.fi>
2025-01-31 14:22:50 +0200

    Created userRegistration signature.

--- src/main/java/org/metropolia/minimalnotepad/service/AuthenticationService.java
+++ src/main/java/org/metropolia/minimalnotepad/service/AuthenticationService.java
@@ -1,8 +1,19 @@
 package org.metropolia.minimalnotepad.service;
 
+import org.metropolia.minimalnotepad.repository.UserRepository;
+import org.metropolia.minimalnotepad.utils.JwtUtils;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+    private UserRepository userRepository;
+    private JwtUtils jwtUtils;
+    private PasswordEncoder passwordEncoder;
 
+    public String authenticate(String username, String password) {
+
+        return jwtUtils.generateToken(username);
+    }
 }
