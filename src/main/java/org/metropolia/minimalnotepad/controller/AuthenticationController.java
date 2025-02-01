864236e1e7c1d3225da0c29f6d18bb57a08f7274
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-01 15:04:50 +0200

    Setting up authentication.

---
+++ src/main/java/org/metropolia/minimalnotepad/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package org.metropolia.minimalnotepad.controller;
+
+import org.metropolia.minimalnotepad.dto.AuthenticationResponse;
+import org.metropolia.minimalnotepad.dto.ErrorResponse;
+import org.metropolia.minimalnotepad.dto.LoginRequest;
+import org.metropolia.minimalnotepad.dto.RegisterRequest;
+import org.metropolia.minimalnotepad.service.AuthenticationService;
+import org.metropolia.minimalnotepad.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usersAuthentication")
+public class AuthenticationController {
+    private final UserService userService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(UserService userService, AuthenticationService authenticationService) {
+        this.userService = userService;
+        this.authenticationService = authenticationService;
+    }
+    @GetMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try
+        {
+            String jwt = authenticationService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
+            return ResponseEntity.ok(new AuthenticationResponse(jwt, loginRequest.getUsername()));
+        }catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(401, e.getMessage()));
+        }
+    }
+    @GetMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest)
+    {
+
+    }
+
+}
--- src/main/java/org/metropolia/minimalnotepad/dto/LoginRequest.java
+++ src/main/java/org/metropolia/minimalnotepad/dto/LoginRequest.java
@@ -1,14 +1,14 @@
 package org.metropolia.minimalnotepad.dto;
 
 public class LoginRequest {
-    private String email;
+    private String username;
     private String password;
-    public LoginRequest(String email, String password) {
-        this.email = email;
+    public LoginRequest(String username, String password) {
+        this.username = username;
         this.password = password;
     }
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
     public String getPassword() {
         return password;
