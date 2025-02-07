cb1ce17b69fc96eb768cc52c886488d80a4f1b61
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-07 19:51:30 +0200

    Add .env variable for the secret key.

--- src/main/java/org/metropolia/minimalnotepad/MinimalnotepadApplication.java
+++ src/main/java/org/metropolia/minimalnotepad/MinimalnotepadApplication.java
@@ -10,7 +10,7 @@ public class MinimalnotepadApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
+        System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
         SpringApplication.run(MinimalnotepadApplication.class, args);
     }
 
--- src/main/java/org/metropolia/minimalnotepad/utils/JwtUtils.java
+++ src/main/java/org/metropolia/minimalnotepad/utils/JwtUtils.java
@@ -3,6 +3,7 @@ package org.metropolia.minimalnotepad.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,12 @@ import java.util.function.Function;
 public class JwtUtils {
     // nrMilliseconds * nrSeconds * nrMinutes * nrHours * nrDays
     private final int timeValidity = 1000 * 60 * 60 * 24;
-    private final String secretKey = "d6e5aea775fad8923f14755ac497bf51ca4e06a576be3a7a1d7583c888170cf0c59db49baed7e6257c8d54e54b0b2a05a74e961462f570b7028b98da327021fe96bc7cba96f57e4dd7ee3ef81efaeb2954bc495fd28b7d60f18d4c75bbd1e1ad7534989d9760d0bde5e8ccb45915f4efac29f70cc11e39c52ee9149bf8eb4e20e8c3de1a65e952ef117e75c500ce8b5b64959e0cc29d35796a9d22dfd63ed2dab2f2cd69be9f11126d1dbc42f2194df2b9db49997a0d35ba64f4c72dda5102ae536861e4e0635f8e1236c6d2dee204125f189703e1c3bbeab77e87f2a6cb656e7ac531b4933653c74b142ee9b52b5e7e41c27d5177555ba3f72f88c8298d2ec3";
+    @Value("${myapp.secret-key}")
+    private String secretKey;
     
 
     public String generateToken(String username) {
+        System.out.println(secretKey);
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
