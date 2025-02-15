983c599fc1699e90499b6fdc53a912124ce311e6
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-15 13:35:10 +0200

    Created NoteCategories to setup JSON connection between the notes and categories.

---
+++ src/main/java/org/metropolia/minimalnotepad/model/NoteCategories.java
@@ -0,0 +1,38 @@
+package org.metropolia.minimalnotepad.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "note_categories")
+public class NoteCategories {
+    @EmbeddedId
+    private NoteCategoriesId id = new NoteCategoriesId();
+    @ManyToOne
+    @MapsId("note_id")
+    @JoinColumn(name = "note_id")
+    @JsonBackReference("category-note")
+    private Note note;
+    @ManyToOne
+    @MapsId("category_id")
+    @JoinColumn(name = "category_id")
+    @JsonBackReference("category-note")
+    private Category category;
+
+    public NoteCategories() {
+
+    }
+    public Note getNote() {
+        return note;
+    }
+    public void setNote(Note note) {
+        this.note = note;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
