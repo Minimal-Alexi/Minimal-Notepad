f1d89b48f5c012e50416c04db161f45aa5c6f7a2
Minimal-Alexi <alexpop@metropolia.fi>
2025-02-21 14:02:22 +0200

    Setting up the SearchUtils function.

---
+++ src/main/java/org/metropolia/minimalnotepad/utils/SearchUtils.java
@@ -0,0 +1,14 @@
+package org.metropolia.minimalnotepad.utils;
+
+import java.util.ArrayList;
+
+public class SearchUtils {
+    public SearchUtils() {
+
+    }
+    public ArrayList<Integer> searchText(ArrayList<String> listOfStringsToSearch, String searchText) {
+        ArrayList<Integer> stringPositions = new ArrayList<>();
+        // Code implementation
+        return stringPositions;
+    }
+}
