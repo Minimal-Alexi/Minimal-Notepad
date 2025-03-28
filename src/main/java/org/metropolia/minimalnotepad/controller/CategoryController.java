2ce93fa4d03df3cedd49775ba5af394fcb4eb823
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-28 13:42:38 +0200

    Set up the retrieval of the categories by language.

--- src/main/java/org/metropolia/minimalnotepad/controller/CategoryController.java
+++ src/main/java/org/metropolia/minimalnotepad/controller/CategoryController.java
@@ -1,9 +1,12 @@
 package org.metropolia.minimalnotepad.controller;
 
 import org.metropolia.minimalnotepad.model.Category;
+import org.metropolia.minimalnotepad.model.User;
 import org.metropolia.minimalnotepad.service.CategoryService;
 
 import org.metropolia.minimalnotepad.service.LanguageService;
+import org.metropolia.minimalnotepad.service.UserService;
+import org.metropolia.minimalnotepad.utils.JwtUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,26 +16,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
-    private final LanguageService languageService;
+    private final UserService userService;
     private final CategoryService categoryService;
+    private final JwtUtils jwtUtils;
 
-    public CategoryController(CategoryService categoryService, LanguageService languageService) {
-        this.languageService = languageService;
+    public CategoryController(CategoryService categoryService, UserService userService, JwtUtils jwtUtils) {
         this.categoryService = categoryService;
+        this.userService = userService;
+        this.jwtUtils = jwtUtils;
     }
 
     @GetMapping
-    public List<Category> getAllCategories(@RequestParam(required = false) Long languageId) {
-        if (languageId != 1) {
-
-            return categoryService.getAllCategories(languageService.getLanguageById(languageId));
-        }
-        return categoryService.getAllCategories();
+    public List<Category> getAllCategories(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = getTokenFromHeader(authorizationHeader);
+        User user = userService.getUserFromToken(token);
+        return categoryService.getAllCategories(user.getLanguage());
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        return categoryService.getCategoryById(id);
+    public Category getCategoryById(@RequestHeader("Authorization") String authorizationHeader,@PathVariable Long id) {
+        String token = getTokenFromHeader(authorizationHeader);
+        User user = userService.getUserFromToken(token);
+        return categoryService.getCategoryById(id, user.getLanguage());
     }
 
     @PostMapping
@@ -52,5 +57,15 @@ public class CategoryController {
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
         return ResponseEntity.noContent().build();
+    }
+    private String getTokenFromHeader(String authorizationHeader) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Authorization header is invalid");
+        }
+        return authorizationHeader.substring(7);
+    }
+    private User getUserFromToken(String token) {
+        String username = jwtUtils.extractUsername(token);
+        return userService.getUserByUsername(username);
     }
 }
--- src/main/java/org/metropolia/minimalnotepad/service/CategoryService.java
+++ src/main/java/org/metropolia/minimalnotepad/service/CategoryService.java
@@ -15,22 +15,24 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
-
+    @Deprecated
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
     public List<Category> getAllCategories(Language language) {
         List<Category> allCategories = categoryRepository.findAll();
-        if (language.getId() != 1) {
-            for (Category category : allCategories) {
-                category.setNameToTranslation(language);
-            }
+        for (Category category : allCategories) {
+            category.setNameToTranslation(language);
         }
         return allCategories;
     }
 
     public Category getCategoryById(Long id, Language language) {
-        return categoryRepository.findById(id).orElse(null); // Using orElse on Optional<Category>
+        Category category =  categoryRepository.findById(id).orElse(null);
+        if (category != null) {
+            category.setNameToTranslation(language);
+        }
+        return category;
     }
     public Category getCategoryById(Long id) {
         return categoryRepository.findById(id).orElse(null); // Using orElse on Optional<Category>
